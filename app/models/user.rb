22ab424242b91b0c47c06914f1d29022ec4f5004
yuuki.satou <yah_satoyuki_0116@yahoo.co.jp>
2018-07-30 01:53:12 +0000

    user login

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  # name のバリデーション
+  validates :name, presence: true, length: { maximum: 50 }
+  # email のバリデーション
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    uniqueness: { case_sensitive: false }
+  # パスワードを暗号化して保存する
+  has_secure_password
+end
