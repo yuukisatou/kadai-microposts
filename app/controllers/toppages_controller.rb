15ab0c4659c6e80458b2f60bf77aae2451604824
yuuki.satou <yah_satoyuki_0116@yahoo.co.jp>
2018-08-03 10:44:09 +0000

    timeline

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -3,7 +3,7 @@ class ToppagesController < ApplicationController
     if logged_in?
       @user = current_user
       @micropost = current_user.microposts.build #form_for用
-      @microposts = current_user.microposts.order("created_at DESC").page(params[:page]) #一覧表示用
+      @microposts = current_user.feed_microposts.order("created_at DESC").page(params[:page]) #一覧表示用
     end
   end
 end
