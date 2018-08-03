7e5d6d19fd26de7cddfe6c38d039815a813cd59a
yuuki.satou <yah_satoyuki_0116@yahoo.co.jp>
2018-08-03 10:30:40 +0000

    follow, unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,25 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverses_of_relatioship, class_name: "Relationship", foreign_key: "follow_id"
+  has_many :followers, through: :reverses_of_relatioship, source: :user
+  
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+  
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+  
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+  
+  
 end
